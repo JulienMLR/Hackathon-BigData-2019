adcb978841ed2bbc135be453771025553d0af42e
Sébastien Thibault <sebastien.thibault@telecomnancy.eu>
2019-11-20 09:57:14 +0100

    Total 3 en utf-16

--- TreeTest.py
+++ TreeTest.py
@@ -1,57 +1,42 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 19 15:10:34 2019
-
-@author: THIBAULT Sébastien
-"""
-
-import numpy as np
-import pandas as pd
-from IPython.display import clear_output
-from matplotlib import pyplot as plt
-
-df = pd.read_csv('ressources/Traite/Finished/InfoVis2017.csv')
-y = dftrain.pop('COMMENT_NOUS_AVEZ_VOUS_DECOUVERT')
-
-
--------------------- COLLAB
-
 import numpy as np
 import pandas as pd
 import io
 from IPython.display import clear_output
 from matplotlib import pyplot as plt
-import requests
 
-url = 'https://raw.githubusercontent.com/SergeGuillemart/Hackathon-BigData-2019/master/ressources/Traite/Finished/InfoVis2017.csv'
+f = open(here +'\\ressources\\Traite\\Finished\\Total3.csv',encoding="utf-16")
 
-myfile = requests.get(url)
-
-open('./test.csv', 'wb').write(myfile.content)
-
-f = open('./test.csv')
-
-
-df = pd.read_csv(f,sep=',')
+df = pd.read_csv(f)
 
 df.pop('DATE')
 df.pop('MIN')
-df.pop('CODE')
+df.pop('CODE_POSTAL')
 df.pop('LOCALITE')
+
+df.pop('LANGUE1')
+df.pop('LANGUE2')
+df.pop('LANGUE3')
+df.pop('LANGUE4')
+df.pop('LANGUE5')
+#df.pop('KM')
+df.pop('PAYS')
 df = df.dropna()
 y = df.pop('COMMENT_NOUS_AVEZ_VOUS_DECOUVERT')
+
+print(y)
+display(df)
 
 
 from sklearn import tree
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.tree.export import export_text
 
-enc = OneHotEncoder(handle_unknown='ignore')
-enc.fit(df)
-print(enc.transform(df).toarray())
+#enc = OneHotEncoder(handle_unknown='ignore')
 #display(df)
 
-#clf = tree.DecisionTreeClassifier()
-#clf = clf.fit(df, y)
-#tree.plot_tree(clf.fit(df,y)) 
+clf = tree.DecisionTreeClassifier(max_depth=5)
+clf = clf.fit(df, y)
+tree.plot_tree(clf.fit(df,y)) 
 
-
+print(export_text(clf))
